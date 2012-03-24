28888bbb11c611d7186eb6ce491ebe2d3c918af1
Sean Madden <sean@seanmadden.net>
2012-03-24 16:56:27 -0400

    Snapshot

---
+++ src/main.cpp
@@ -0,0 +1,52 @@
+/*
+ * main.cpp
+ *
+ *    Copyright (C) 2012 Sean P Madden
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *    If you would like to license this code under the GNU LGPL, please see
+ *    http://www.seanmadden.net/licensing for details.
+ *
+ */
+#include <boost/program_options.hpp>
+#include <string>
+
+namespace po = boost::program_options;
+
+int main(int argc, char** argv) {
+	po::options_description options;
+	options.add_options()
+			("address,a", po::value<std::string>("224.0.0.251"), "multicast address")
+			("port,p", po::value<int>(5353), "Port to xmit on")
+			("help,h", "print this message");
+
+	po::variables_map map;
+	try {
+		po::store(po::parse_command_line(argc, argv, options), map);
+		po::notify(map);
+	} catch (const po::invalid_option_value& e) {
+		std::cerr << "Invalid value for: " << e.get_option_name() << std::endl;
+	} catch (const po::multiple_values& e){
+		std::cerr << "Too many values for: " << e.get_option_name() << std::endl;
+	}
+
+	if (map.count("help")) {
+		options.print(std::cout);
+		return 0;
+	}
+
+	return 0;
+}
+
